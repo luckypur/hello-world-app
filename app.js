b54599bf1ea149d75ebe601e40ec91b2f5c8951e
satyajeet <luckypur@gmail.com>
2024-11-11 20:44:16 +0530

    asf

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello Worlssd!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
